a4d10e3cbe0bfa4c7e7cf2b05e8c603e7c5b78ec
Lord MQ <weirdvk@hotmail.com>
2025-01-28 23:56:29 +0000

    add more debugging and remove secrets debugging

--- main.py
+++ main.py
@@ -12,4 +12,5 @@ if __name__ == "__main__":
 
     # Set the port and host
     port = int(os.getenv("PORT", 5000))
+    print(f"Starting Flask app on port {port}...")
     app.run(host="0.0.0.0", port=port)
